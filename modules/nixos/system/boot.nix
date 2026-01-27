d84efd92f9baeccb2d40ff7b04def26adcf06a30
pingu01 <gabrielmnascimento04@gmail.com>
2026-01-26 23:08:30 -0300

    add: configure limine bootloader with secure boot and custom entries

--- modules/nixos/system/boot.nix
+++ modules/nixos/system/boot.nix
@@ -1,7 +1,29 @@
-{ ... }:
+{ pkgs, ... }:
 {
-boot.loader = {
-  systemd-boot.enable = true;
-  efi.canTouchEfiVariables = true;
-   };
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.limine = {
+    enable = true;
+    secureBoot.enable = true;
+    maxGenerations = 3;
+    extraEntries = ''
+      /Windows 11
+        protocol: efi
+        path: guid(beb32385-b5db-4e1c-aca6-5262fd83cd45):/EFI/Microsoft/Boot/bootmgfw.efi
+      /memtest86
+        protocol: efi
+        path: boot():/limine/efi/memtest86/memtest86.efi
+    '';
+    additionalFiles = {
+      "efi/memtest86/memtest86.efi" = "${pkgs.memtest86-efi}/BOOTX64.efi";
+    };
+    style = {
+      wallpapers = [ ];
+      interface = {
+        resolution = "1920x1080";
+        helpHidden = true;
+        branding = "pingu's workstation";
+        brandingColor = 6;
+      };
+    };
+  };
 }
